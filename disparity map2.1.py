d621b5ea89cba1acc8f8c5441604f1f770f8ee1d
jli262 <lijiayi262@gmail.com>
2021-05-08 18:03:53 +0800

    dd

--- disparity map2.1.py
+++ disparity map2.1.py
@@ -2,30 +2,20 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-# Read both images and convert to grayscale
-img1 = cv.imread(r'D:\Disparity-Map-Generator\StereoMatchingTestings\Art\view1.png', cv.IMREAD_GRAYSCALE)
-img2 = cv.imread(r'D:\Disparity-Map-Generator\StereoMatchingTestings\Art\view5.png', cv.IMREAD_GRAYSCALE)
+img1 = cv.imread(r'D:\Disparity-Map-Generator\StereoMatchingTestings\Reindeer\view1.png', cv.IMREAD_GRAYSCALE)
+img2 = cv.imread(r'D:\Disparity-Map-Generator\StereoMatchingTestings\Reindeer\view5.png', cv.IMREAD_GRAYSCALE)
 
-# 1. Detect keypoints and their descriptors
-# Based on: https://docs.opencv.org/master/dc/dc3/tutorial_py_matcher.html
-
-# Initiate SIFT detector
 sift = cv.SIFT_create()
-# find the keypoints and descriptors with SIFT
+
 kp1, des1 = sift.detectAndCompute(img1, None)
 kp2, des2 = sift.detectAndCompute(img2, None)
 
-# Match keypoints in both images
-# Based on: https://docs.opencv.org/master/dc/dc3/tutorial_py_matcher.html
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)   # or pass empty dictionary
 flann = cv.FlannBasedMatcher(index_params, search_params)
 matches = flann.knnMatch(des1, des2, k=2)
 
-# Keep good matches: calculate distinctive image features
-# Lowe, D.G. Distinctive Image Features from Scale-Invariant Keypoints. International Journal of Computer Vision 60, 91–110 (2004). https://doi.org/10.1023/B:VISI.0000029664.99615.94
-# https://www.cs.ubc.ca/~lowe/papers/ijcv04.pdf
 matchesMask = [[0, 0] for i in range(len(matches))]
 good = []
 pts1 = []
@@ -33,36 +23,23 @@ pts2 = []
 
 for i, (m, n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
-        # Keep this keypoint pair
         matchesMask[i] = [1, 0]
         good.append(m)
         pts2.append(kp2[m.trainIdx].pt)
         pts1.append(kp1[m.queryIdx].pt)
 
-# ------------------------------------------------------------
-# STEREO RECTIFICATION
-
-# Calculate the fundamental matrix for the cameras
-# https://docs.opencv.org/master/da/de9/tutorial_py_epipolar_geometry.html
 pts1 = np.int32(pts1)
 pts2 = np.int32(pts2)
 fundamental_matrix, inliers = cv.findFundamentalMat(pts1, pts2, cv.FM_RANSAC)
 
-# We select only inlier points
 pts1 = pts1[inliers.ravel() == 1]
 pts2 = pts2[inliers.ravel() == 1]
 
-# Visualize epilines
-# Adapted from: https://docs.opencv.org/master/da/de9/tutorial_py_epipolar_geometry.html
-
-
 def drawlines(img1src, img2src, lines, pts1src, pts2src):
-    ''' img1 - image on which we draw the epilines for the points in img2
-        lines - corresponding epilines '''
     r, c = img1src.shape
     img1color = cv.cvtColor(img1src, cv.COLOR_GRAY2BGR)
     img2color = cv.cvtColor(img2src, cv.COLOR_GRAY2BGR)
-    # Edit: use the same random seed so that two images are comparable!
+
     np.random.seed(0)
     for r, pt1, pt2 in zip(lines, pts1src, pts2src):
         color = tuple(np.random.randint(0, 255, 3).tolist())
@@ -73,69 +50,35 @@ def drawlines(img1src, img2src, lines, pts1src, pts2src):
         img2color = cv.circle(img2color, tuple(pt2), 5, color, -1)
     return img1color, img2color
 
-
-# Find epilines corresponding to points in right image (second image) and
-# drawing its lines on left image
 lines1 = cv.computeCorrespondEpilines(
     pts2.reshape(-1, 1, 2), 2, fundamental_matrix)
 lines1 = lines1.reshape(-1, 3)
 img5, img6 = drawlines(img1, img2, lines1, pts1, pts2)
 
-# Find epilines corresponding to points in left image (first image) and
-# drawing its lines on right image
 lines2 = cv.computeCorrespondEpilines(
     pts1.reshape(-1, 1, 2), 1, fundamental_matrix)
 lines2 = lines2.reshape(-1, 3)
 img3, img4 = drawlines(img2, img1, lines2, pts2, pts1)
 
-# Stereo rectification (uncalibrated variant)
-# Adapted from: https://stackoverflow.com/a/62607343
 h1, w1 = img1.shape
 h2, w2 = img2.shape
 _, H1, H2 = cv.stereoRectifyUncalibrated(
     np.float32(pts1), np.float32(pts2), fundamental_matrix, imgSize=(w1, h1)
 )
 
-# Rectify (undistort) the images and save them
-# Adapted from: https://stackoverflow.com/a/62607343
 img1_rectified = cv.warpPerspective(img1, H1, (w1, h1))
 img2_rectified = cv.warpPerspective(img2, H2, (w2, h2))
 
-# # Draw the rectified images
-# fig, axes = plt.subplots(1, 2, figsize=(15, 10))
-# axes[0].imshow(img1_rectified, cmap="gray")
-# axes[1].imshow(img2_rectified, cmap="gray")
-# axes[0].axvline(250)
-# axes[1].axvline(250)
-# axes[0].axvline(450)
-# axes[1].axvline(450)
-# plt.suptitle("Rectified images")
-# plt.show()
+block_size = 15
+min_disp = -96
+max_disp = 112
 
-# ------------------------------------------------------------
-# CALCULATE DISPARITY (DEPTH MAP)
-# Adapted from: https://github.com/opencv/opencv/blob/master/samples/python/stereo_match.py
-# and: https://docs.opencv.org/master/dd/d53/tutorial_py_depthmap.html
-
-# StereoSGBM Parameter explanations:
-# https://docs.opencv.org/4.5.0/d2/d85/classcv_1_1StereoSGBM.html
-
-# Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range.
-block_size = 3
-min_disp = -144
-max_disp = 176
-# Maximum disparity minus minimum disparity. The value is always greater than zero.
-# In the current implementation, this parameter must be divisible by 16.
 num_disp = max_disp - min_disp
-# Margin in percentage by which the best (minimum) computed cost function value should "win" the second best value to consider the found match correct.
-# Normally, a value within the 5-15 range is good enough
+
 uniquenessRatio = 5
-# Maximum size of smooth disparity regions to consider their noise speckles and invalidate.
-# Set it to 0 to disable speckle filtering. Otherwise, set it somewhere in the 50-200 range.
+
 speckleWindowSize = 0
-# Maximum disparity variation within each connected component.
-# If you do speckle filtering, set the parameter to a positive value, it will be implicitly multiplied by 16.
-# Normally, 1 or 2 is good enough.
+
 speckleRange = 2
 disp12MaxDiff = 0
 
@@ -152,8 +95,7 @@ stereo = cv.StereoSGBM_create(
 )
 disparity_SGBM = stereo.compute(img1_rectified, img2_rectified)
 
-# Normalize the values to a range from 0..255 for a grayscale image
 disparity_SGBM = cv.normalize(disparity_SGBM, disparity_SGBM, alpha=255,
                               beta=0, norm_type=cv.NORM_MINMAX)
 disparity_SGBM = np.uint8(disparity_SGBM)
-cv.imwrite("D:/Disparity-Map-Generator/PSNR_Assignment2/2.1 output/disp1.png", disparity_SGBM)
+cv.imwrite(r"D:\Disparity-Map-Generator\PSNR_Assignment2\PSNR_Python\pred\Reindeer\disp1.png", disparity_SGBM)
